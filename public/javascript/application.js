6e984554f76315d1825fc83287e97d1f1277613f
Sunil Unka <sunil.unka@gmail.com>
2014-08-14 18:16:40 +0000

    Initial commit of API app.

---
+++ public/javascript/application.js
@@ -0,0 +1,12 @@
+function initialize() {
+    var mapOptions = {
+      center: new google.maps.LatLng(-34.397, 150.644),
+      zoom: 8
+    };
+    var map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
+};
+
+$(document).ready(function() {
+   google.maps.event.addDomListener(window, 'load', initialize); 
+ 
+});
